491e18ce36c3b77eec2fd75566191fe15ba33e2c
HimhMinhHieu <trinhminhhieu2712@gmail.com>
2024-05-09 16:26:34 +0700

    delete

--- Reducer/userReducer/userReducer.js
+++
@@ -1,9 +0,0 @@
-export default userReducer = (currentState, action) => {
-    switch(action.type){
-        case 'login':
-            return action.payload;
-        case 'logout':
-            return null;
-    }
-    return currentState;
-}
